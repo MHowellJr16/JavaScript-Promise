414538fa016453365fd28baf58e175f9a127a915
Mark <mhowelljr16@gmail.com>
2025-02-10 17:51:21 -0500

    Feb. 10_4:59 time mark

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+// 2 ways to use FETCH
+// THEN METHOD:
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => (
+    console.log(response)
+))
+// TIME STAMP: 4:59
