ceba57241c3295b0fdc973f741f95e36beefc5e8
Mark <mhowelljr16@gmail.com>
2025-02-11 16:03:59 -0500

    Feb. 11_Then_Async

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 // 2 ways to use FETCH
-// THEN METHOD:
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => (
-    console.log(response)
+const emailRef = (document.querySelector('.email'))
+console.log(emailRef)
+// THEN METHOD: 1st way
+/**  fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => (
+    response.json().then(data => {
+        console.log(data)
+        emailRef.innerHTML = data.name
+    })
 ))
-// TIME STAMP: 4:59
+*/
+// THEN METHOD: 2nd way
+/** fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then((response) => {
+    return response.json()
+    })
+    .then(data => {
+    console.log(data)
+    })
+*/
+// Async/Await Method:
+async function main() {
+const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+const data = await response.json()
+console.log(data)
+emailRef.innerHTML = data.email
+}
+main()
+//TIME STAMP: 17:55
