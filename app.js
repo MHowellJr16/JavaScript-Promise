87969ec2ff8ec277213637f8e36d9af10c7d0942
Mark <mhowelljr16@gmail.com>
2025-02-12 11:47:30 -0500

    Feb. 12_27:13

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/**
+ * PROMISE: Something that gives you VALUE some time in the FUTURE
+ */
+const statusRef = document.querySelector('.status')
+const videoRef = document.querySelector()
 console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 // 2 ways to use FETCH
 const emailRef = (document.querySelector('.email'))
@@ -20,11 +25,47 @@ console.log(emailRef)
     })
 */
 // Async/Await Method:
-async function main() {
+/**async function main() {
 const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
 const data = await response.json()
 console.log(data)
 emailRef.innerHTML = data.email
 }
 main()
+*/
 //TIME STAMP: 17:55
+// Creating NEW PROMISE
+function getSubsriptionStatus() { 
+    return new Promise((resolve, reject) => {
+        resolve("VIP")
+    })
+}
+// METHOD 1: Then
+// getSubsriptionStatus().then(response => console.log(response))
+// METHOD 2: Async/Await 
+/** 
+async function main() {
+    const status = (await getSubsriptionStatus()) 
+    statusRef.innerHTML = status
+}
+main()
+*/
+function getVideo(subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if(subscriptionStatus === 'VIP') {
+            resolve("show video")
+        }
+        else if(subscriptionStatus === 'FREE') {
+            resolve("show trailer")
+        }
+        else {
+            reject('no video')
+        }
+    })
+}
+async function main() {
+    const status = (await getSubsriptionStatus()) 
+    statusRef.innerHTML = status
+    console.log(await getVideo(status))
+}
+main()
